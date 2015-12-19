afbccb308818a4194308d98de6c371446ce088ae
Jinyue Song <jinyuesong88@gmail.com>
2015-12-19 16:04:13 -0500

    main

---
+++ main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  FinalProj
+//
+//  Created by Jinyue Song on 2015-12-19.
+//  Copyright © 2015 Jinyue Song. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "structs.h"
+
+#ifdef __APPLE__
+#   include <OpenGL/gl.h>
+#   include <OpenGL/glu.h>
+#   include <GLUT/glut.h>
+#else
+#   include <GL/gl.h>
+#   include <GL/glu.h>
+#   include <GL/freeglut.h>
+#endif
+
+
