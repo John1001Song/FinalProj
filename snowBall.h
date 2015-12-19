86de58197666bb92af4cfbeb055c1644f0ee7d2d
Richard Damra <damrarv@mcmaster.ca>
2015-12-19 18:53:37 -0500

    Added class functions, prototype only so far

--- snowBall.h
+++ snowBall.h
@@ -13,9 +13,42 @@
 #include "snow.h"
 #include "structs.h"
 
+
+#ifndef _BULLET_H_
+#define _BULLET_H_
+
+using namespace std;
+
+static int LIFE = 2000;
+
 class SnowBall: public Snow{
 public:
     SnowBall();//constructor
+	
+	SnowBall(float *origin, float* dir);
+	virtual ~Snowball();
+	
+	/* May or may not need the follow function*/
+	void set_Dir(float* direction);
+	float* get_Dir();
+
+	float* get_Pos();
+	void set_Pos(float* position);
+
+	int get_Size() const;
+	void set_Size(int size);
+
+	float get_Speed() const;
+	void set_Speed(float speed);
+
+	int get_Life() const;
+	void set_Life(int life);
+
+	void update();//update snowball as it travels may or may not need this
+
+	bool is_Hit(float* target, int size);
+
+	static void normalize(float* vec);
     
     Vector3D color;
     
@@ -28,5 +61,13 @@ public:
     void draw();//draw a snowball
     
     bool checkDis();//this func is used for check distance between this snowball and other snowmen
+	
+	
+private:
+	float position[3];
+	int size;
+	float direction[3];
+	float speed;
+	int life;
 };
 #endif /* snowBall_h */
