ccec1c31f147acaa83fa91339f15a281a6be9afe
Jinyue Song <jinyuesong88@gmail.com>
2015-12-19 12:32:03 -0500

    rock and roll

---
+++ snowman.h
@@ -0,0 +1,14 @@
+//
+//  snowman.h
+//  FinalProj
+//
+//  Created by Jinyue Song on 2015-12-19.
+//  Copyright © 2015 Jinyue Song. All rights reserved.
+//
+
+#ifndef snowman_h
+#define snowman_h
+
+#include <stdio.h>
+
+#endif /* snowman_hpp */
