7e7e6be4eb225832728cee455969135c3038ba59
Ding, Xuchao <dingx3@mcmaster.ca>
2015-12-19 20:40:13 -0500

    add singleton Game class and global variables

---
+++ Game.cpp
@@ -0,0 +1,9 @@
+//
+//  main.cpp
+//  FinalProj
+//
+//  Created by Jinyue Song on 2015-12-19.
+//  Copyright © 2015 Jinyue Song. All rights reserved.
+//
+
+#include "Game.h"
---
+++ Game.h
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FinalProj
+//
+//  Created by Jinyue Song on 2015-12-19.
+//  Copyright © 2015 Jinyue Song. All rights reserved.
+//
+#include <vector>
+#include "SnowBall.h"
+#include "SnowMan.h"
+
+extern vector<SnowBall> SnowBallListAI;
+extern vector<SnowBall> SnowBallListPlayer;
+extern vector<SnowMan> SnowManList;
