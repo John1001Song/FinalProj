02f560f7a3484e7693771fd621c2cc97aff890a2
Jinyue Song <jinyuesong88@gmail.com>
2015-12-20 14:34:58 -0500

    header of snowman

--- snowMan.h
+++ snowMan.h
@@ -24,11 +24,33 @@ public:
     SnowMan(ManType whatType);//constructor
     ManType manType;
     
+    virtual ~SnowMan();//deconstructor
+    
     //we could use draw() to draw different size snowman base on its type
     virtual void draw();
     
-    //we could use snowOperate1() to let snowman shoots snowball
+    //we could use fire() to let snowman shoots snowball
     void fire();
+    
+    void setPos(float *pos);
+    float* getPos();
+    
+    void setDir(float *dir);
+    float* getDir();
+    
+    void setAngle(float angle);
+    float getAngle();
+    
+    void update();
+    
+    
+    
+    
+private:
+    float pos[3];
+    float dir[3];
+    float angle;
+    
 };
 
 #endif /* snowMan_h */
